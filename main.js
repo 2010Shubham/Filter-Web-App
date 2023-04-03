f81987f6c3aa1cc8cf6d192a83691b30778a17b8
2010Shubham <85053325+2010Shubham@users.noreply.github.com>
2023-04-03 10:59:26 +0545

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(500, 500);
+    canvas.center();
+
+
+}
+
+function draw(){
+    background("#bfbfff");
+}
+
+function take_snapshot(){
+    save("snapshot.png");
+}
