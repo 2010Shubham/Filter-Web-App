4be2d785f5689c1b630aecb35388106cde123d79
2010Shubham <85053325+2010Shubham@users.noreply.github.com>
2023-04-05 09:22:56 +0545

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,45 @@
-function preload(){
+var x = 0;
+var y = 0;
+
+
+
+function preload() {
 
 }
 
-function setup(){
+function setup() {
     canvas = createCanvas(500, 500);
     canvas.center();
-
+    video = createCapture(VIDEO);
+    video.hide();
+    my_poseNet = ml5.poseNet(video, modelLoaded);
+    my_poseNet.on('pose', gotposes);
 
 }
 
-function draw(){
+function draw() {
     background("#bfbfff");
+    image(video, 0, 0, 500, 500);
+
 }
 
-function take_snapshot(){
+function take_snapshot() {
     save("snapshot.png");
+}
+
+
+function modelLoaded() {
+    console.log("Model is loaded");
+}
+
+
+function gotposes(results) {
+ 
+    if (results.length > 0) {
+
+        x = results[0].pose.nose.x;
+        y = results[0].pose.nose.y;
+        console.log("Nose X is " + x + " and Y is " + y);
+    }
+
 }
